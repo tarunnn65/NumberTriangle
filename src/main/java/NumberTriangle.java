f780b39255c2f31bff51075eb0595b0da1f1929f
tarunnn65 <kstarunraj@gmail.com>
2025-09-26 17:43:37 -0400

    Completed Static Code

--- src/main/java/NumberTriangle.java
+++ src/main/java/NumberTriangle.java
@@ -91,17 +91,6 @@ public class NumberTriangle {
      *
      */
     public int retrieve(String path) {
-        if (path.equals("")) {
-            return this.root;
-        }
-
-        else if (path.substring(0, 1).equals("l")) {
-            return left.retrieve(path.substring(1));
-        }
-
-        else if (path.substring(0,1).equals("r")) {
-            return right.retrieve(path.substring(1));
-        }
         return -1;
     }
 
